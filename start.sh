23f1bc4b3c5f79c35c62208ae3b72d4c57dbe425
smaug <smaug.jiao@topnetwork.org>
2021-01-21 11:20:30 +0800

    update start.sh/stop.sh/restart.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,5 @@ sh ./stop.sh
 
 cd /home/topuser/smaug/top-argus-agent/
 source  vvlinux/bin/activate
-cd agent
-#nohup python argus_agent.py -a 127.0.0.1:9090 -f ./xtop.log > /dev/null & 2>&1
-nohup python argus_agent.py -a $1 -f $2 > /dev/null & 2>&1
+python3  main.py -a $1 -f $2
 echo "done start"
--- stop.sh
+++ stop.sh
@@ -1,1 +1,1 @@
-kill -9 `ps -ef |grep argus_agent.py |grep -v grep |awk -F ' ' '{print $2}'`
+kill -9 `ps -ef |grep topargus-agent |grep -v grep |awk -F ' ' '{print $2}'`
